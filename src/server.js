f8ff24c5cd055814f5afa721198d0bccc3a187bf
Johan Ekström <johan.ekstrom@live.se>
2024-01-21 19:57:37 +0100

    Add getallbyid endpoint

--- src/server.js
+++ src/server.js
@@ -2,8 +2,13 @@ import express from 'express'
 import fruitBoxes from './data.js'
 const app = express()
 
-app.get("/api/fruitbox/", (req, res) => {
+app.get("/api/fruitbox/getAll", (req, res) => {
     res.json(fruitBoxes)
+})
+
+app.get("/api/fruitbox/getAllById/:id", (req, res) => {
+    const filteredFruitboxes = fruitBoxes.filter(f => f.Id == req.params.id);
+    res.json(filteredFruitboxes)
 })
 
 app.listen(8001, () => console.log("Server up and running"))
