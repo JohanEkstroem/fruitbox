709dcce6fd4d5538cb522501ed04b4425d688227
Johan Ekström <johan.ekstrom@live.se>
2024-01-21 20:04:13 +0100

    Add fruitbox endpoint

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import fruitBoxes from './data.js'
+import { uuid } from 'uuidv4'
 const app = express()
 
 app.get("/api/fruitbox/getAll", (req, res) => {
@@ -9,6 +10,11 @@ app.get("/api/fruitbox/getAll", (req, res) => {
 app.get("/api/fruitbox/getAllById/:id", (req, res) => {
     const filteredFruitboxes = fruitBoxes.filter(f => f.Id == req.params.id);
     res.json(filteredFruitboxes)
+})
+
+app.post("api/fruitbox/addFruitBox", () => {
+    fruitBoxes.push({ Id: uuid, Name: req.body.name, Articles: req.body.articles })
+    res.json(req.body).status(201)
 })
 
 app.listen(8001, () => console.log("Server up and running"))
